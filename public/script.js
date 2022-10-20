192fbe29a0397f11f20ad26c25757fd022592539
PxO Ink LLC <git@pxoink.net>
2022-10-19 23:38:13 -0400

    Update script to properly add records via html.

--- public/script.js
+++ public/script.js
@@ -1,48 +1,98 @@
-var addRecordButton = document.getElementById('addRecord');
+var triggerAddRecordButtonListener = function() {
+	var addRecordButtons = document.getElementsByClassName('addRecord');
 
-addRecordButton.addEventListener('click', function(e) {
-	e.preventDefault();
+	for(var i = 0; i < addRecordButtons.length; i++) {
+		var addRecordButton = addRecordButtons[i];
 
-	var index = parseInt(document.getElementsByClassName('addRecord').length);
+		addRecordButton.addEventListener('click', function(e) {
+			e.preventDefault();
 
-	if (isNaN(index) || index < 1) {
-		index = 0;
+			var parent = this.parentNode;
+			var index = parseInt(parent.parentNode.getElementsByClassName('addRecordField').length);
+
+			if (isNaN(index) || index < 1) {
+				index = 0;
+			}
+
+			var div = document.createElement('div');
+			div.className = "addRecordField";
+
+			var labelName = document.createElement('label');
+			var labelNameContent = document.createTextNode('Record Name #' + (index + 1));
+			labelName.setAttribute('for', 'addRecordName' + index);
+			labelName.appendChild(labelNameContent);
+
+			div.appendChild(labelName);
+
+			var inputName = document.createElement('input');
+			inputName.setAttribute('id', 'addRecordName' + index)
+			inputName.setAttribute('name', 'addRecordName[' + index + ']');
+			inputName.setAttribute('type', 'text');
+			inputName.setAttribute('size', 40);
+
+			div.appendChild(inputName);
+
+			var labelContent = document.createElement('label');
+			var labelContentContent = document.createTextNode('Record Content #' + (index + 1));
+			labelContent.setAttribute('for', 'addRecordContent' + index);
+			labelContent.appendChild(labelContentContent);
+
+			div.appendChild(labelContent);
+
+			var inputContent = document.createElement('textarea');
+			inputContent.setAttribute('id', 'addRecordContent' + index)
+			inputContent.setAttribute('name', 'addRecordContent[' + index + ']');
+			inputContent.setAttribute('rows', 5);
+			inputContent.setAttribute('cols', 42);
+
+			div.appendChild(inputContent);
+
+			parent.parentNode.insertBefore(div, parent);
+		}, false);
 	}
+}();
 
-	var div = document.createElement('div');
-	div.className = "addRecord";
+var archives = document.getElementsByClassName('archive');
 
-	var labelName = document.createElement('label');
-	var labelNameContent = document.createTextNode('Record Name #' + (index + 1));
-	labelName.setAttribute('for', 'addRecordName' + index);
-	labelName.appendChild(labelNameContent);
+if (archives.length > 0) {
+	for(i = 0; i < archives.length; i++) {
+		var archive = archives[i];
+		var endpoint = archive.dataset.endpoint;
+		var header = archive.getElementsByTagName('h3')[0];
 
-	div.appendChild(labelName);
+		header.addEventListener('click', function(e) {
+			e.preventDefault();
 
-	var inputName = document.createElement('input');
-	inputName.setAttribute('id', 'addRecordName' + index)
-	inputName.setAttribute('name', 'addRecordName[' + index + ']');
-	inputName.setAttribute('type', 'text');
-	inputName.setAttribute('size', 40);
+			var request = new XMLHttpRequest();
 
-	div.appendChild(inputName);
+			request.onreadystatechange = function() {
+				if (request.readyState === XMLHttpRequest.DONE) {
+					if (request.status !== 200) {
+						if (confirm("There was an error communicating with the application. Refresh the page to try again?")) {
+							location.reload(true);
+						}
+					} else {
+						response = JSON.parse(request.responseText);
 
-	var labelContent = document.createElement('label');
-	var labelContentContent = document.createTextNode('Record Content #' + (index + 1));
-	labelContent.setAttribute('for', 'addRecordContent' + index);
-	labelContent.appendChild(labelContentContent);
+	console.log(response);
 
-	div.appendChild(labelContent);
+						if (!response.data) {
+							if (confirm("There was an error retrieving the archive data. Refresh the page to try again?")) {
+								location.reload(true);
+							}
+						} else {
+							document.getElementById('archive').remove();
 
-	var inputContent = document.createElement('textarea');
-	inputContent.setAttribute('id', 'addRecordContent' + index)
-	inputContent.setAttribute('name', 'addRecordContent[' + index + ']');
-	inputContent.setAttribute('rows', 5);
-	inputContent.setAttribute('cols', 42);
+							var div = document.createElement('div');
+							div.id = "archive";
+						}
 
-	div.appendChild(inputContent);
+					}
+				}
+			};
 
-	addRecordButton.parentNode.insertBefore(div, addRecordButton);
-}, false);
-
-var addRecordTemplk
+			request.open('GET', endpoint);
+			request.send();
+		});
+	}
+}
